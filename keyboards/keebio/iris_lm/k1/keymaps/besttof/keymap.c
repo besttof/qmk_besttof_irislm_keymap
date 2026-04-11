9d2b96e4195fbdcc699c6b7e671e8ebdc995a294
Hessel <hessel@besttof.com>
2026-04-11 14:13:03 +0200

    Parent macros and default to macOS layout

--- keyboards/keebio/iris_lm/k1/keymaps/besttof/keymap.c
+++ keyboards/keebio/iris_lm/k1/keymaps/besttof/keymap.c
@@ -17,6 +17,11 @@ enum custom_keycodes {
     ARWPTR,
     KILLAPP,
 
+    // Parenthesis, Bracket, Curly Bracket, Angle Bracket macros
+    IN_PRN,
+    IN_BRC,
+    IN_CBR,
+    IN_ABK,
 
     // Color settings
     COL_1,
@@ -30,14 +35,14 @@ enum custom_keycodes {
 
 #define HOME_S LALT_T(KC_S)
 #define HOME_D LSFT_T(KC_D)
-#define HOME_F LCTL_T(KC_F)
-#define HOME_V LGUI_T(KC_V)
+#define HOME_F LGUI_T(KC_F)
+#define HOME_V LCTL_T(KC_V)
 #define HOME_G MEH_T(KC_G)
 
 #define HOME_L RALT_T(KC_L)
 #define HOME_K RSFT_T(KC_K)
-#define HOME_J RCTL_T(KC_J)
-#define HOME_M RGUI_T(KC_M)
+#define HOME_J RGUI_T(KC_J)
+#define HOME_M RCTL_T(KC_M)
 #define HOME_H MEH_T(KC_H)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_MINS, KC_Z,    KC_X,    KC_C,    HOME_V,  KC_B,    KC_HOME,          KC_END,  KC_N,    HOME_M,  KC_COMM, KC_DOT,  KC_SLSH, TO_SYS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, TO_SYM,  KC_ENT,                    KC_BSPC, TO_NAV,  KC_RALT
+                                    KC_HYPR, TO_SYM,  KC_ENT,                    KC_BSPC, TO_NAV,  KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -60,11 +65,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                             KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KILLAPP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_F11,  KC_F12,  _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     _______, KC_F11,  KC_F12,  _______, _______, _______,                            IN_BRC,  IN_PRN,  IN_CBR,  IN_ABK,  _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, KC_LALT, KC_LSFT, KC_LCTL, KC_MEH,                             KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, _______,
+     _______, _______, KC_LALT, KC_LSFT, KC_LGUI, KC_MEH,                             KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     ARWPTR,  _______, _______, _______, _______, CW_TOGG, _______,          _______, KC_LPRN, KC_RPRN,   KC_LT,  KC_GT,   _______, ARWLMDA,
+     ARWPTR,  _______, _______, _______, KC_LCTL, _______, CW_TOGG,          _______, KC_LPRN, KC_RPRN, KC_LT,   KC_GT,   _______, ARWLMDA,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -88,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      EE_CLR,  _______, _______, RM_TOGG, _______, _______,                            _______, _______, KC_MUTE, _______, _______, KC_SLEP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     COL_1,   _______, _______, RM_VALU, _______,  DT_UP,                              _______, KC_MSTP, KC_VOLU, KC_MPLY, _______, KC_BRMU,
+     COL_1,   _______, _______, RM_VALU, _______,  DT_UP,                             _______, KC_MSTP, KC_VOLU, KC_MPLY, _______, KC_BRMU,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      COL_2,   _______, RM_PREV, RM_VALD, RM_NEXT, DT_DOWN,                            _______, KC_MPRV, KC_VOLD, KC_MNXT, _______, KC_BRMD,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -99,23 +104,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+
+// MACROS
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case ARWLMDA:
-            if (record->event.pressed) {
-                SEND_STRING("=>");
-            } else {
-                // when keycode ARWLMDA is released
-            }
+            if (record->event.pressed) SEND_STRING("=>");
             break;
 
         case ARWPTR:
-            if (record->event.pressed) {
-                SEND_STRING("->");
-            } else {
-                // when keycode ARWPTR is released
-            }
+            if (record->event.pressed) SEND_STRING("->");
             break;
+
+        case IN_PRN:
+           if (record->event.pressed) SEND_STRING("()"SS_TAP(X_LEFT));
+           break;
+
+        case IN_BRC:
+           if (record->event.pressed) SEND_STRING("[]"SS_TAP(X_LEFT));
+           break;
+
+        case IN_CBR:
+           if (record->event.pressed) SEND_STRING("{}"SS_TAP(X_LEFT));
+           break;
+
+        case IN_ABK:
+           if (record->event.pressed) SEND_STRING("<>"SS_TAP(X_LEFT));
+           break;
 
         case COL_1:
             if (record->event.pressed) {
@@ -143,17 +159,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 switch (detected_host_os()) {
                     case OS_MACOS:
-                        //   tap_code16(G(KC_Q));
-                        SEND_STRING("MAC");
+                    case OS_IOS:
+                        tap_code16(G(KC_Q));
                         break;
                     case OS_WINDOWS:
-                        //   tap_code16(A(KC_F4));
-                        SEND_STRING("WIN");
+                        tap_code16(A(KC_F4));
                         break;
-                    case OS_IOS:
                     case OS_LINUX:
                     case OS_UNSURE:
-                        SEND_STRING("IDK");
                         break;
                 }
             } else {
